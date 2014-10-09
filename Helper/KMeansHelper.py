7ffe41733d1b7f0f9e91509d2c1a87b32186bd0f
Thomas Pinetz <thomaspinetz@gmail.com>
2014-10-09 17:53:25 +0200

    Set up some standard Stuff

---
+++ Helper/KMeansHelper.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+class KMeansHelper:
+    def __init__(self, clusters):
+        self.clusters = clusters
+
+    def runKMeans(self,data):
+        raise NotImplementedError
--- main.py
+++ main.py
@@ -1,6 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from Helper.KMeansHelper import KMeansHelper
+
 
 def main():
-  print("hello world!")
-
+    kmeansHelper = KMeansHelper(3)
+    dataset = datasets.load_iris()
+    #kmeansHelper.runKMeans(dataset)
+    data = np.array(dataset["data"])
+    #print(dataset)
+    plt.plot(data)
+    #for key in dataset:
+        #print(dataset[key])
+    
 if __name__ == "__main__":
-  main()
+    main()
